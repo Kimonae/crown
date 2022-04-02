ca6b69480af2033a09a50e2fc495be3873383955
Kimonae <70099141+Kimonae@users.noreply.github.com>
2022-04-02 23:29:29 +0200

    Add files via upload

---
+++ produits.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Produits royaux</title>
+            <meta name="author" content="Saku">
+            <meta name="description" content="Produits du site de Couronne-Royauté.">
+            <link rel="icon" href="../img/cha.jpg" />
+            <link rel="stylesheet" href="../style.css"/>
+        </head>
+
+        
+
+    <header>
+
+<?php 
+
+ include("../header.php");
+
+?>
+    
+    </header>
+
+    <body>
+
+    <h2> Nous ferons de vous le roi que vous méritez d'être </h2>
+
+            
+    <span id="prodposition"> 
+    <p> Retrouvez nos produits en ligne </p>
+
+
+</span>
+
+    <div id = "Products">
+
+    <?php
+
+    
+
+
+?>
+   
+
+
+</div>
+        </body>
+
+
+        <footer>
+            
+        <?php
+        include("../footer.php");
+
+        ?>
+        </footer>
+
+        </html>
